5ac56da5a9cbb4fae581c19dada07bf82bc6a404
Deisel <74831846+Amelia-was@users.noreply.github.com>
2021-02-19 08:59:43 -0500

    post notes

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,11 +1,20 @@
 const router = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
 
 const { notes } = require('../../data/db');
+const { createNote } = require('../../lib/notes');
 
 router.get('/notes', (req, res) => {
     let results = notes;
     
     res.json(results);
 });
+
+router.post('/notes', (req, res) => {
+    req.body.id = uuidv4();
+
+    const note = createNote(req.body, notes);
+    res.json(note)
+})
 
 module.exports = router;
